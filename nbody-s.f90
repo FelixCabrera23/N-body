b727cd42fab735b1883a628e87250273d4a85baa
FelixCabrera23 <nerd.occa@gmail.com>
2020-09-19 21:17:53 -0600

    fortran funcions

---
+++ nbody-s.f90
@@ -0,0 +1,15 @@
+! 2020 - 14 - 5
+! nbody-s.f90
+! Félix Cabrera (walberto.cabrera@gmail.com)
+
+! Simulación de N cuerpos bajo un potencial gravitatorio
+! por medio de la conservación del momentum angular
+
+! Codificación del texto: UTF-8
+! Compiladores probados: GNU Fortran (Ubuntu 9.3.0-10ubuntu2) 9.3.0
+
+
+PROGRAM nbody
+
+
+END PROGRAM nbody
